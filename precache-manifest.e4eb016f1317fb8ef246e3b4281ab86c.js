bb317315de9180c7e0408980b5ecc97c2f8c3090
Luca Ricci <luca.ricci@traderlink.it>
2019-02-20 01:31:09 +0100

    tic tac toe implementation
    some simple react app

---
+++ precache-manifest.e4eb016f1317fb8ef246e3b4281ab86c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "87fa1c7ae1d263b4708c",
+    "url": "/static/js/main.87fa1c7a.chunk.js"
+  },
+  {
+    "revision": "74002baa0a6f80b9b4ef",
+    "url": "/static/js/2.74002baa.chunk.js"
+  },
+  {
+    "revision": "87fa1c7ae1d263b4708c",
+    "url": "/static/css/main.053ec54e.chunk.css"
+  },
+  {
+    "revision": "d3575fb575a4154434afa059c0c8e63a",
+    "url": "/index.html"
+  }
+];
